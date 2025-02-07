d594e2ab5b6ed8e56e934248c606e448c08da499
Natalia Romashev <tatasay@mail.ru>
2025-02-07 13:15:27 +0300

    Перенесена логика в src/index.js, добавлен импорт и вызов функции

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-import _ from "lodash";
-console.log(_.last(['one', 'two']));
+// import _ from "lodash";
+// console.log(_.last(['one', 'two']));
+import getBirthYear from './src/index.js';
+
+console.log('Сколько вам лет?');
+const age = 18; // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
+const birthYear = getBirthYear(age);
+console.log(`Ваш год рождения: ${birthYear}`);
